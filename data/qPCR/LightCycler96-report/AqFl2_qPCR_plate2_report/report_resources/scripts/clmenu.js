55e4117a98a33e683bc7ec1479019398be728012
yanxianl <yanxianl@nmbu.no>
2019-08-22 12:00:41 +0200

    upload raw and clean data of qPCR

---
+++ data/qPCR/LightCycler96-report/AqFl2_qPCR_plate2_report/report_resources/scripts/clmenu.js
@@ -0,0 +1,49 @@
+// JavaScript for collapsing / expanding menu
+function toggleMenu(objID) {
+	if (!document.getElementById) return;
+	var ob = document.getElementById(objID).style;
+	var img = document.getElementById("expandStateIcon");
+	if (ob.display == 'block')
+	{
+		ob.display = 'none';
+		img.src = "report_resources/images/expand_icon.png";
+	}
+	else
+	{
+		ob.display = 'block';
+		img.src = "report_resources/images/collapse_icon.png";
+	}
+}
+
+function Initialize(linkid){
+var elem = document.getElementById(linkid);
+if (typeof elem.onclick == "function") {
+    elem.onclick.apply(elem);
+}
+
+if (elem.tagName == "A")
+{
+  report = document.getElementById("report_viewer");
+  report.src = elem.href;
+}
+}
+
+function selectItem(itemId, parentContainerId)
+{
+  var elem = document.getElementById(parentContainerId);
+  if (elem)
+  {	
+     var items = elem.getElementsByTagName("a");
+	 for (var i=0; i < items.length; i++) 
+	 {
+        if (items[i].id == itemId)
+		{
+			items[i].className  = "selected";
+		}
+		else
+		{
+			items[i].className  = "";
+		}
+	 }
+  }
+}
---
+++ data/qPCR/README.md
@@ -0,0 +1,2 @@
+The qPCR assay reports are located in the `LightCycler96-report` folder. 
+The qPCR setup and plate calibration are documented in the excel sheets.

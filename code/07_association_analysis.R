57a5fbc95fd41f21d29310dec9faaece3258273f
yanxianl <yanxianl@nmbu.no>
2019-08-22 12:27:34 +0200

    delete unwanted files

--- code/07_association_analysis.R
+++
@@ -1,8 +0,0 @@
-library(Maaslin2)
-
-input_data <- system.file(
-  'extdata','example1_features.txt', package="Maaslin2")
-input_metadata <-system.file(
-  'extdata','example1_metadata.txt', package="Maaslin2")
-fit_data <- Maaslin2(
-  input_data, input_metadata,'maaslin2_example_output')
